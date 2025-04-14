3cea64dacd9bb0a660c1ec13b50ae4549e0b013b
bagasdprs <bagasdprs@gmail.com>
2025-04-14 21:42:26 +0700

    update detail page and create checkout page

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home route
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home');
+
+// Detail route
+Route::get('/detail', [DetailController::class, 'index'])
+    ->name('detail');
 
 // Route::resource('pages.admin', DashboardController::class);
 Route::prefix('admin')
     ->namespace('Admin')
     ->group(function () {
-        // Route::get('/', 'DashboardController@index')
-        //     ->name('dashboard');
         Route::get('/', [DashboardController::class, 'index'])
             ->name('dashboard');
     });
